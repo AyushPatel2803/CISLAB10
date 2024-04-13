26b1677589425ce787dab528522894f49c6c5449
Ayush Patel <address>
2024-04-13 14:20:50 -0400

    changes made

--- src/TestSelectionSort.java
+++ src/TestSelectionSort.java
@@ -15,29 +15,30 @@ public class TestSelectionSort {
 
     public void testPositive() {
         int[] arr = {11, 19, 17, 20, 12};
-        int[] sortedArr = {2, 7, 8, 9, 10};
+        int[] sortedArr = {11, 12, 17, 19, 20};
         selectionSort.basicSelectionSort(arr);
         Assert.assertArrayEquals("Positive numbers not sorted correctly", sortedArr, arr);
     }
+    
 
 
     public void testNegative() {
         int[] arr = {-18, -19, -17, -20, -12};
-        int[] sortedArr = {-10, -9, -8, -7, -2};
+        int[] sortedArr = {-20, -19, -18, -17,-12};
         selectionSort.basicSelectionSort(arr);
         Assert.assertArrayEquals("Negative numbers not sorted correctly", sortedArr, arr);
     }
 
     public void testMixed() {
         int[] arr = {15, -13, 20, -14, 12};
-        int[] sortedArr = {-4, -3, 0, 2, 5};
+        int[] sortedArr = {-14, -13, 12, 15, 20};
         selectionSort.basicSelectionSort(arr);
         Assert.assertArrayEquals("Mixed numbers not sorted correctly", sortedArr, arr);
     }
 
     public void testDuplicates() {
         int[] arr = {15, 13, 15, -11, 20, -11};
-        int[] sortedArr = {-1, -1, 0, 3, 5, 5};
+        int[] sortedArr = {-11, -11, 13, 15, 15, 20};
         selectionSort.basicSelectionSort(arr);
         Assert.assertArrayEquals("Array with duplicates not sorted correctly", sortedArr, arr);
     }
